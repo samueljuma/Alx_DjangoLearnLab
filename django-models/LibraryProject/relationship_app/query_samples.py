76fa5385f97d20cae6220e3d888732ae14ce0224
samueljuma <samueljuma1024@gmail.com>
2025-02-23 21:15:17 +0300

    update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -19,10 +19,8 @@ def list_books_in_library(library_name):
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)  
-    librarian = Librarian.objects.filter(library=library).first()
-    if librarian:
-        print(f"Librarian of {library_name}: {librarian.name}")
-    else:
-        print(f"No librarian found for {library_name}")
+    library = Library.objects.get(name=library_name) 
+    librarian = Librarian.objects.get(library=library) 
+    print(f"Librarian of {library_name}: {librarian.name}")
+
 
