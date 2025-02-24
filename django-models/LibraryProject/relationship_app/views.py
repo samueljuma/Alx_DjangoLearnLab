08fd9fc0dd4cd6bfb2c977c0d1d465fa663d953b
samueljuma <samueljuma1024@gmail.com>
2025-02-24 08:54:21 +0300

    update solution for Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,5 +13,5 @@ def list_books(request):
 # Class Based Views
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
