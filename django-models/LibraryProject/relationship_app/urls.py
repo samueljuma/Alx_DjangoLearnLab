32e91dfe8e977482c1c8df15490a9b647048db0e
samueljuma <samueljuma1024@gmail.com>
2025-02-25 08:01:54 +0300

    update solution for wk9 task 4:  Implementing Custom Permissions in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
     
-    path('books/add/', add_book, name="add_book"),
-    path('books/edit/<int:book_id>/', edit_book, name="edit_book"),
+    path('books/add/', add_book, name="add_book/"),
+    path('books/edit/<int:book_id>/', edit_book, name="edit_book/"),
     path('books/delete/<int:book_id>/', delete_book, name='delete_book'), 
 ]
