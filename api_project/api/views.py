d343db6d77206d6cb384004437ef230bca20b301
samueljuma <samueljuma1024@gmail.com>
2025-03-08 15:45:19 +0300

    add custom permission

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,16 +6,25 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import IsAdminUser
+from .permissions import IsStaffOrReadOnly # a custom permission. 
+
 
 class BookList(generics.ListAPIView):
-  queryset = Book.objects.all()
-  serializer_class = BookSerializer
-  authentication_classes = [TokenAuthentication]
-  permission_classes = [IsAuthenticated]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+# class BookViewSet(viewsets.ModelViewSet):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [IsAdminUser, IsAuthenticated]
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAdminUser, IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsStaffOrReadOnly]
