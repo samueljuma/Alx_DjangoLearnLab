eaa31d0ecb1dee38916413d57239d147e0c2b0e4
samueljuma <samueljuma1024@gmail.com>
2025-03-12 09:55:00 +0300

    update directories

---
+++ advanced_api_project/advanced-api-project/api/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Author, Book
+from .forms import BookAdminForm
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+  list_display = ('name', 'get_books')
+  
+  def get_books(self, obj):
+        """Return a comma-separated list of books written by the author."""
+        return ", ".join([book.title for book in obj.books.all()])
+    
+  get_books.short_description = 'Books'
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+  form = BookAdminForm
+  list_display = ('title', 'publication_year', 'author')
