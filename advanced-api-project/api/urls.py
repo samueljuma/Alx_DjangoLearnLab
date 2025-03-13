04de0f24b3cfc2bebf92647aa3ebab810479929a
samueljuma <samueljuma1024@gmail.com>
2025-03-13 08:51:07 +0300

    update views

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import BookListCreateView, BookDetailView
+from .views import BookCreateView, BookDetailView, BookListView, BookDeleteView, BookUpdateView
 
 urlpatterns = [
-    path("api/v1/books/", BookListCreateView.as_view(), name="book-list-create"),
-    path("api/v1/books/<int:pk>/", BookDetailView.as_view(), name="book-detail")
+    path("api/v1/books/", BookListView.as_view(), name="book-list"),
+    path("api/v1/books/add/", BookCreateView.as_view(), name="book-add"),
+    path("api/v1/books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
+    path("api/v1/books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update"),
+    path("api/v1/books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,12 +3,44 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-# list all books and create books
-class BookListCreateView(generics.ListCreateAPIView):
-  queryset = Book.objects.all()
-  serializer_class = BookSerializer
-  
-# Retrieve, update or delete a book by id
-class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
-  queryset = Book.objects.all()
-  serializer_class = BookSerializer
+
+# list all books
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+# Create a book
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+# Get Book by ID
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+# Update Book
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+# Delete Book
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+# # list all books and create books
+# class BookListCreateView(generics.ListCreateAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+
+# # Retrieve, update or delete a book by id
+# class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
