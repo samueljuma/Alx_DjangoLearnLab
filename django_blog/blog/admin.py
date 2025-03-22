06076185bb5bc00ba1b688a8693ae2c1dd04ec08
samueljuma <samueljuma1024@gmail.com>
2025-03-22 19:13:56 +0300

    add Comment model

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Profile
+from .models import Post, Profile, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -12,3 +12,9 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ["user", "joined_date", "profile_pic"]
     search_fields = ["user"]
     list_filter = ["joined_date"]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["post", "author", "created_at"]
+    search_fields = ["post", "author"]
+    list_filter = ["created_at"]
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -7,6 +7,9 @@ class Post(models.Model):
   published_date = models.DateTimeField(auto_now_add=True)
   author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="posts" )
   
+  def __str__(self):
+    return self.title
+  
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
   bio = models.TextField()
@@ -15,3 +18,13 @@ class Profile(models.Model):
   
   def __str__(self):
     return f"{self.user.username}'s profile"
+
+class Comment(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  content = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  
+  def __str__(self):
+    return f"{self.author.username}'s comment on {self.post.title}"
