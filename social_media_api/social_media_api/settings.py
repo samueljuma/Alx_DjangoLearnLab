ee244c67f127c7f0f7419019e649be2b6811a427
samueljuma <samueljuma1024@gmail.com>
2025-03-28 14:20:34 +0300

    configure db

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = "social_media_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "socialmedia_api ",
+        "USER": "root",
+        "PASSWORD": "sj_root",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
